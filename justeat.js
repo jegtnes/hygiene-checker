0b75978931b62ac46614300e96b980f9cf47d4bb
jegtnes <alex.jegtnes@cxpartners.co.uk>
2016-10-02 11:58:22 +0100

    hot damn I think it works

--- justeat.js
+++ justeat.js
@@ -1,1 +1,26 @@
-document.body.style.border = "5px solid blue";
+if (window.fetch) {
+  console.log('fetch');
+  const baseURL = 'https://fsaapiwrapper.jegtnes.com/establishments?address=';
+  let headers = new Headers();
+  headers.append('Content-Type', 'application/json');
+
+  let requestSettings = {
+    method: 'GET',
+    headers: headers,
+    mode: 'cors',
+  };
+
+  let address = encodeURIComponent(document.querySelector('p.address').innerText);
+
+  let fullURL = baseURL + address;
+
+  fetch(fullURL, requestSettings).then((response) => {
+    console.log(response);
+    return response.text();
+  }).then((text) => {
+    console.log('food hygiene rating: ' + text);
+  });
+
+} else {
+  console.log('stop trying to make fetch happen');
+}
