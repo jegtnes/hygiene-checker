06d0fe6ae9b971d37e9245762eac3006bccaa09e
jegtnes <alex.jegtnes@cxpartners.co.uk>
2016-10-02 18:52:59 +0100

    Refactor for future improvements

--- justeat.js
+++ justeat.js
@@ -11,14 +11,11 @@ if (window.fetch) {
   };
 
   let address = encodeURIComponent(document.querySelector('p.address').innerText);
+  const postcode = encodeURIComponent(document.querySelector('#postcode').innerText);
 
   let fullURL = baseURL + address;
 
-  fetch(fullURL, requestSettings).then((response) => {
-    console.log(response);
-    return response.json();
-  }).then((responseObject) => {
-
+  function insertTakeoutRating(takeoutData) {
     const justEatRestaurantOverview = document.getElementsByClassName('restaurantOverview')[0];
     const container = document.createElement('div');
     const image = document.createElement('img');
@@ -26,29 +23,36 @@ if (window.fetch) {
     container.classList.add('o-card');
     container.style.padding = '16px';
 
-    if (responseObject.SchemeType === 'FHRS') {
+    if (takeoutData.SchemeType === 'FHRS') {
       // England, Wales, NI
-      const ratingValue = parseInt(responseObject.RatingValue, 10);
+      const ratingValue = parseInt(takeoutData.RatingValue, 10);
 
       if (ratingValue >= 0 && ratingValue <= 5) {
         image.src = `${baseImgURL}${ratingValue}.jpg`;
       } else {
         console.log('edge case?', ratingValue);
       }
-    } else if (responseObject.SchemeType === 'FHIS') {
-      if (responseObject.RatingValue === 'Pass') {
+    } else if (takeoutData.SchemeType === 'FHIS') {
+      if (takeoutData.RatingValue === 'Pass') {
         image.src = `${baseImgURL}scot_pass.jpg`
-      } else if (responseObject.RatingValue === 'Pass and Eat Safe') {
+      } else if (takeoutData.RatingValue === 'Pass and Eat Safe') {
         image.src = `${baseImgURL}scot_passandeatsafe.jpg`
-      } else if (responseObject.RatingValue === 'Improvement Required') {
+      } else if (takeoutData.RatingValue === 'Improvement Required') {
         image.src = `${baseImgURL}scot_improvementrequired.jpg`
       } else {
-        console.log('scottish edge case?', responseObject.RatingValue);
+        console.log('scottish edge case?', takeoutData.RatingValue);
       }
     }
 
     container.appendChild(image);
     justEatRestaurantOverview.parentNode.insertBefore(container, justEatRestaurantOverview.nextSibling);
+  }
+
+  fetch(fullURL, requestSettings).then((response) => {
+    console.log(response);
+    return response.json();
+  }).then((responseObject) => {
+    insertTakeoutRating(responseObject)
   });
 
 } else {
