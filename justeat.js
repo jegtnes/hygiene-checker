7edd43e693ac1250e40268d5dee8d65b4ed5d2da
jegtnes <alex.jegtnes@cxpartners.co.uk>
2016-10-02 14:31:59 +0100

    Added functionality for England, Wales, NI

--- justeat.js
+++ justeat.js
@@ -1,6 +1,6 @@
 if (window.fetch) {
-  console.log('fetch');
   const baseURL = 'https://fsaapiwrapper.jegtnes.com/establishments?address=';
+  const baseImgURL = 'http://static.jegtnes.co.uk/hygienechecker/';
   let headers = new Headers();
   headers.append('Content-Type', 'application/json');
 
@@ -16,9 +16,32 @@ if (window.fetch) {
 
   fetch(fullURL, requestSettings).then((response) => {
     console.log(response);
-    return response.text();
-  }).then((text) => {
-    console.log('food hygiene rating: ' + text);
+    return response.json();
+  }).then((responseObject) => {
+    if (responseObject.SchemeType === 'FHRS') {
+      const ratingValue = parseInt(responseObject.RatingValue, 10);
+      console.log(ratingValue);
+      // England, Wales, NI
+
+      let justEatRestaurantOverview = document.getElementsByClassName('restaurantOverview')[0];
+
+      if (ratingValue >= 0 && ratingValue <= 5) {
+        let container = document.createElement('div');
+        container.classList.add('o-card');
+        container.style.padding = '16px';
+
+        let image = document.createElement('img');
+        image.src = `${baseImgURL}${ratingValue}.jpg`;
+
+        container.appendChild(image);
+
+        justEatRestaurantOverview.parentNode.insertBefore(container, justEatRestaurantOverview.nextSibling);
+      }
+    } else if (responseObject.SchemeType === 'FHIS') {
+      // Scotland
+
+    }
+    console.log(responseObject);
   });
 
 } else {
